662f8cd63041f344feb2d73724e9c87574a861d1
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-09-29 13:23:59 -0400

    Copied RoverRuckus repository

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ItWorrrrrrrrrkeeedddd.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "It worrrrrrrrrkeeedddd", group = "Testing")
+public class ItWorrrrrrrrrkeeedddd extends OpMode {
+    public void init() {}
+
+
+    public void loop() {
+        telemetry.addLine("I'm aliiiiiiiiiive");
+    }
+}
