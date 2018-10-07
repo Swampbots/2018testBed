f93e20d91a790a1e87ccfc3b54cac4e2df04fb8a
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-10-06 21:11:59 -0400

    Removed whitespace

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleBlueVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleBlueVision.java
@@ -1,41 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
 import org.corningrobotics.enderbots.endercv.OpenCVPipeline;
-
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-
 import org.opencv.core.MatOfPoint;
-
 import org.opencv.core.Scalar;
-
 import org.opencv.core.Size;
-
 import org.opencv.imgproc.Imgproc;
 
-
 import java.util.ArrayList;
-
 import java.util.List;
-
 
 /**
  * Created by guinea on 10/5/17.
-
  * -------------------------------------------------------------------------------------
- *
- Copyright (c) 2018 FTC Team 5484 Enderbots
- *
+ * Copyright (c) 2018 FTC Team 5484 Enderbots
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -43,38 +33,26 @@ import java.util.List;
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
- *
+ * 
+ * 
  * By downloading, copying, installing or using the software you agree to this license.
  * If you do not agree to this license, do not download, install,
  * copy or use the software.
-
  * -------------------------------------------------------------------------------------
- *
-
- A nice demo class for using OpenCVPipeline. This one also demonstrates how to use OpenCV to threshold
+ * A nice demo class for using OpenCVPipeline. This one also demonstrates how to use OpenCV to threshold
  * for a certain color (blue) and find contours of objects of that color, which is very common in
  * robotics OpenCV applications.
  */
 
-
-
 public class ExampleBlueVision extends OpenCVPipeline {
-
     private boolean showContours = true;
-
     // To keep it such that we don't have to instantiate a new Mat every call to processFrame,
     // we declare the Mats up here and reuse them. This is easier on the garbage collector.
-
     private Mat hsv = new Mat();
-
     private Mat thresholded = new Mat();
 
-
-    // this is just here so we can expose it later through getContours.
-
+    // this is just here so we can expose it later thru getContours.
     private List<MatOfPoint> contours = new ArrayList<>();
-
 
     public synchronized void setShowCountours(boolean enabled) {
         showContours = enabled;
