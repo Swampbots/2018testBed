8e5dc4063140236197ad68949c07158e2a2a22db
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-10-08 14:31:40 -0400

    Added debugging telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldPipelineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldPipelineTest.java
@@ -7,12 +7,34 @@ import org.corningrobotics.enderbots.endercv.CameraViewDisplay;
 
 @Autonomous(name = "Gold Pipeline Test", group = "Testing")
 public class GoldPipelineTest extends LinearOpMode {
+
     private GoldPipeline vision;
+
     @Override
     public void runOpMode() {
+
+        telemetry.addLine("Creating pipeline instance...");
+        telemetry.update();
+        sleep(2500);
+
         vision = new GoldPipeline();
+        telemetry.addLine("GoldPipeline instance created.");
+        telemetry.update();
+        sleep(2500);
+
         vision.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+
+        telemetry.addLine("Vision initialized.");
+        telemetry.update();
+        sleep(2500);
+
         vision.enable();
+
+        telemetry.addLine("Vision enabled.");
+        telemetry.addLine("Press play to start.");
+        telemetry.update();
+        sleep(2500);
+
 
         waitForStart();
 
