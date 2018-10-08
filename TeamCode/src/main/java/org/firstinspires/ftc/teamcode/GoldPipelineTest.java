b6ade26151f3d12f0e5795f3703973440a5228d5
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-10-08 11:08:05 -0400

    Added call to vision.init() and vision.enable during initialization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldPipelineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldPipelineTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.corningrobotics.enderbots.endercv.CameraViewDisplay;
@@ -10,8 +11,14 @@ public class GoldPipelineTest extends LinearOpMode {
     @Override
     public void runOpMode() {
         vision = new GoldPipeline();
+        vision.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        vision.enable();
+
+        waitForStart();
+
 
         while(opModeIsActive()) {
+
             // vision.process();
         }
     }
