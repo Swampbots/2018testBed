86f6858e69912afc8b7956a8791eb4c98b281b8f
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-10-19 14:44:53 -0400

    Added waitForStart() before try-catch; appended, flushed and closed writer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WriteToFileAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WriteToFileAuto.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import java.io.*;
 
@@ -8,10 +9,24 @@ import java.io.FileWriter;
 public class WriteToFileAuto extends LinearOpMode {
 
     public void runOpMode() {
+
+        waitForStart();
+
         try {
             FileWriter writer = new FileWriter("Test_File");
+
+            writer.append("Hello");
+            writer.append("\nThis thing working?");
+            writer.flush();
+
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        while(opModeIsActive()) {
+
+        }
+
     }
 }
