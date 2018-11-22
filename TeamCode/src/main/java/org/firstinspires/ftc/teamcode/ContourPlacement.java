9def959f4ea2b711847d02c9a258dfba6c0235d2
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-11-21 23:14:11 -0500

    Added ContourPlacement enum

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ContourPlacement.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+public enum ContourPlacement {
+    // The fields indicate placement of contours in the processed image. The image will be split into three portions.
+    LEFT,
+    CENTER,
+    RIGHT,
+    UNKNOWN;
+
+
+    
+
+}
