e83793c0420d063c43b38f294d6cf1dda341878d
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-11-28 19:04:23 -0500

    Added PID instance and variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverHardware.java
@@ -75,6 +75,17 @@ public class RoverHardware {
 
 
 
+    // Autonomous PID variables
+    public final double MAX_SPEED = 0.4;
+    public final double P = 0.045;
+    public final double I = 0.01;
+    public final double D = 0.045;
+    public final double TOLERANCE = 2;
+
+    public final SynchronousPID pid = new SynchronousPID(P, I, D);
+
+
+
 
     // Motors
     public DcMotor frontLeft;
