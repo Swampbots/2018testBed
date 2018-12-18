8735d304d424906942b1584b098129d4ef2144b0
Blake Sanders <ac3.train3r.blak3@gmail.com>
2018-12-18 13:41:09 -0500

    Removed static keyword from COOLDOWN declaration
    I realized it doesn't always make sense to have this be the same value across all cooldowns, so for now it will go back to being non-static.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonCooldown.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonCooldown.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 public class ButtonCooldown {
 
-    private static final double COOLDOWN = 0.075; // 75 milliseconds
+    private final double COOLDOWN = 0.075; // 75 milliseconds
     private double snapshot = 0.0;
 
     public void updateSnapshot(double runtime) {
